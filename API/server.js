406b6e8776c8371d33a4ad002dc4114b4cd55cbf
Andrej <andrej1360@gmail.com>
2023-08-17 21:18:14 +0200

    Simple mysql update

--- API/server.js
+++ API/server.js
@@ -1,6 +1,88 @@
 const express = require('express');
 const moment = require('moment');
 
+const mysql = require('mysql');
+
+// Create a connection to the MySQL server
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root' // Replace with your MySQL password
+});
+
+// Connect to the MySQL server
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL server');
+    //DROP OLD DATABASE
+    const dropDatabaseQuery = 'DROP DATABASE IF EXISTS new_database';
+    connection.query(dropDatabaseQuery, (err, result) => {
+        if (err) {
+            console.error('Error dropping database:', err);
+        } else {
+            console.log('Database dropped');
+        }
+    });
+    //Create database
+    const databaseName = 'new_database';
+    const createDatabaseQuery = `CREATE DATABASE ${databaseName}`;
+    connection.query(createDatabaseQuery, (err, result) => {
+        if (err) {
+            console.error('Error creating database:', err);
+        } else {
+            console.log(`Database '${databaseName}' created`);
+        }
+    });
+    //select database
+    const useDatabaseQuery = `USE ${databaseName}`;
+    connection.query(useDatabaseQuery, (err, result) => {
+        if (err) {
+            console.error('Error selecting database:', err);
+        } else {
+            console.log(`Database '${databaseName}' selected`);
+        }
+    });   
+    
+    // Create the first table
+    const createLoraQuery = `
+        CREATE TABLE lora (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            idLora INT UNIQUE
+        )
+    `;
+
+    connection.query(createLoraQuery, (err) => {
+        if (err) {
+            console.error('Error creating table lora:', err);
+        } else {
+            console.log('Table lora created');
+        }
+    });
+    // Create the second table
+    const createValuesQuery = `
+        CREATE TABLE val (
+            id INT PRIMARY KEY,
+            tem FLOAT,
+            hum FLOAT,
+            datetime DATETIME,
+            humTempFK INT,
+            FOREIGN KEY (humTempFK) REFERENCES lora(id)
+        )
+    `;
+
+    connection.query(createValuesQuery, (err) => {
+        if (err) {
+            console.error('Error creating table val:', err);
+        } else {
+            console.log('Table val created');
+        }
+    });
+});
+
+
 // Create a new Express application
 const app = express();
 
@@ -39,8 +121,21 @@ app.post('/api/freq', (req, res) => {
 app.post('/api/newID', (req, res) => {
     try{
         if(req.body.id!==""){
-            //update db
-            res.status(200).json({data:'Updated id'});
+            //insert into table lora
+            try{
+                const insertLoraQuery = `INSERT INTO lora (idLora) VALUES (${req.body.id})`;
+                connection.query(insertLoraQuery, (err, result) => {
+                    if (err) {
+                        console.error('Error inserting new id:', err);
+                    } else {
+                        console.log(`New id inserted`);
+                        res.status(200).json({data:'Updated id'});
+                    }
+                });
+            }catch(e){
+                res.status(500).json({data:'Failed to update id: '+e});
+            }
+            
         }else{
             throw 'Invalid id';
         }
@@ -49,4 +144,5 @@ app.post('/api/newID', (req, res) => {
         res.status(500).json({data:'Failed to update id: '+e});
     }
 });
+
 app.listen(3001, () => console.log('API running port 3001.'));
