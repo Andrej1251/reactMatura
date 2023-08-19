4ffaac9d19d78c08243faacf529a7b67c8fde7be
Andrej <andrej1360@gmail.com>
2023-08-19 21:25:23 +0200

    added support for color change

--- API/server.js
+++ API/server.js
@@ -8,7 +8,23 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'root' // Replace with your MySQL password
 });
-
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    //select database
+    const databaseName = 'new_database';
+    const useDatabaseQuery = `USE ${databaseName}`;
+    connection.query(useDatabaseQuery, (err, result) => {
+        if (err) {
+            console.error('Error selecting database:', err);
+        } else {
+            console.log(`Database '${databaseName}' selected`);
+        }
+    });   
+});
+/*
 // Connect to the MySQL server
 connection.connect((err) => {
     if (err) {
@@ -63,7 +79,7 @@ connection.connect((err) => {
     // Create the second table
     const createValuesQuery = `
         CREATE TABLE val (
-            id INT PRIMARY KEY,
+            id INT PRIMARY KEY AUTO_INCREMENT,
             tem FLOAT,
             hum FLOAT,
             datetime DATETIME,
@@ -79,24 +95,55 @@ connection.connect((err) => {
             console.log('Table val created');
         }
     });
-});
+});*/
 
 
 // Create a new Express application
 const app = express();
+var allShapes = [];
+
 
 var intervalStop;
-function updateFreq(updateF){
+function updateFreq(updateF,res){
     if(updateF!=0){
         if(intervalStop){
             clearInterval(intervalStop);
         }
         intervalStop = setInterval(() => {
-            //update db
-            //console.log(updateF);
+            //go to db end get lora idLora
+            const selectLoraQuery = 'SELECT idLora FROM lora';
+            connection.query(selectLoraQuery, (err, result) => {
+                
+                if (err) {
+                    console.error('Error selecting lora:', err);
+                } else {
+                    for(var i=0;i<result.length;i++){
+                        //get random values for now RUN LORA in reality
+                        var tem = Math.random() * (40 - 20) + 20;
+                        var hum = Math.random() * (100 - 0) + 0;
+                        var datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+                        //insert into table val
+                        const insertValQuery = `INSERT INTO val (tem,hum,datetime,humTempFK) VALUES (${tem},${hum},"${datetime}",${result[i].idLora})`;
+                        var b={};
+                        b.idLora=result[i].idLora;
+                        b.temp=tem;
+                        b.humd=hum;
+                        b.datetime=datetime;
+                        connection.query(insertValQuery, (err, result) => {
+                            if (err) {
+                                console.error('Error inserting new value:', err);
+                            } 
+                        });
+                        allShapes.push(b);
+                        //console.log("=====================================")
+                    }
+                }
+                
+            });
         }, updateF);
     }
 }
+
 updateFreq(0);
 // Parse JSON and URL-encoded request bodies
 app.use(express.json());
@@ -108,21 +155,50 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+//server side event
+app.get('/subscribe', (req, res) => {
+    console.log('New client connected!');
+    res.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		Connection: 'keep-alive',
+		'Cache-Control': 'no-cache',
+    });
+    // Send a subsequent message every five seconds
+    setInterval(() => {
+        for(var i=0;i<allShapes.length;i++){
+            //console.log(allShapes[i]);
+            res.write(`data: ${JSON.stringify(allShapes[i])}`);
+            res.write('\n\n');
+        }
+        allShapes=[];
+    }, 1000);
+
+    req.on('close', () => res.end('OK'))
+});
 
 app.post('/api/freq', (req, res) => {
     try{
-        updateFreq(req.body.updateFreq);
+        updateFreq(req.body.updateFreq,res);
         res.status(200).json({data:'Updated frequecy'});
     }catch(e){
         res.status(500).json({data:'Failed to update frequecy: '+e});
     }
 });
+app.post('/api/history', (req, res) => {
+    try{
+        
+        res.status(200).json({data:'Updated history'});
+    }catch(e){
+        res.status(500).json({data:'Failed to update history: '+e});
+    }
+});
+
 app.post('/api/newID', (req, res) => {
     try{
         if(req.body.id!==""){
             //insert into table lora
             try{
-                const insertLoraQuery = `INSERT INTO lora (idLora,name) VALUES (${req.body.id},"${req.body.name}") ON DUPLICATE KEY UPDATE idLora=${req.body.id} AND name="${req.body.name}"`;
+                const insertLoraQuery = `INSERT INTO lora (idLora,name) VALUES (${req.body.id},"${req.body.name}") ON DUPLICATE KEY UPDATE name="${req.body.name}"`;
                 connection.query(insertLoraQuery, (err, result) => {
                     if (err) {
                         console.error('Error inserting new id:', err);
@@ -145,4 +221,5 @@ app.post('/api/newID', (req, res) => {
     }
 });
 
+  
 app.listen(3001, () => console.log('API running port 3001.'));
